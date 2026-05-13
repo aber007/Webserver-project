c0f0ea1b2424672f889456366173782b715532e6
Anton Bergström <aanntonb@gmail.com>
2026-05-13 16:41:06 +0200

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-        session['current_user'] = {
-            'id': user_obj.id,
-            'firstName': user_obj.firstName,
-            'lastName': user_obj.lastName,
-            'email': user_obj.email,
-            'city': user_obj.city,
-            'accountCreated': user_obj.accountCreated
-        }
